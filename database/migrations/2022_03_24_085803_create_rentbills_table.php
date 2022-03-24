b3682417a43f3e32ac8a61cf3735510583682f6a
rxdlcz <delacruzrex14@gmail.com>
2022-03-24 17:41:18 +0800

    Create 2022_03_24_085803_create_rentbills_table.php

---
+++ database/migrations/2022_03_24_085803_create_rentbills_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rentbills', function (Blueprint $table) {
+            $table->id();
+            $table->integer('unit_id');
+            $table->integer('current_balance');
+            $table->date('last_payment');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rentbills');
+    }
+};
