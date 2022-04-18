65cb8f86c6d4f7bde43a357e511e83e519df4182
rxdlcz <delacruzrex14@gmail.com>
2022-04-18 10:31:30 +0800

    Edit Profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,8 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
+use Hash;
+use Session;
 
 class ProfileController extends Controller
 {
-    //
+    public function editProfile(Request $request, $id)
+    {
+        
+        $validator = Validator::make($request->all(), [
+            'firstname' => 'required',
+            'lastname' => 'required',
+            'email' => "required|email|unique:users,email,$id",
+            'username' => "required|unique:users,username,$id",
+        ]);
+
+        if (!$validator->passes()) {
+            return response()->json(['status' => 0, 'error' => $validator->errors()->toArray()]);
+        } else {
+            $user = user::find($id);
+            $user->firstname = $request->firstname;
+            $user->lastname = $request->lastname;
+            $user->email = $request->email;
+            $user->username = $request->username;
+
+            $res = $user->save();
+
+            if ($res) {
+                return response()->json(['status' => 1, 'error' => $validator->errors()->toArray()]);
+            } else {
+                return response()->json(['status' => 0, 'error' => $validator->errors()->toArray()]);
+            }
+        }
+    }
 }
